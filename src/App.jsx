8b2cb87b5252f525100f978c356f3dd925a4a020
koliak <xkoliakx@gmail.com>
2023-09-12 11:44:10 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import HomeScreen from './screens/HomeScreen';
+import './App.css';
+
+
+function App() {
+  return(
+    <HomeScreen />
+  );
+}
+
+export default App;
---
+++ src/screens/HomeScreen/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchBar from "../../components/SearchBar/SearchBar";
+
+
+function HomeScreen () {
+  return (
+    <>
+      <SearchBar />
+    </>
+  )
+}
+
+export default HomeScreen;
